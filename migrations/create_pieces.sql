719aac9f8b51a886ab029605182d44c24f4c37dc
Lin Jiang <jianglin@lnjng.com>
2025-05-02 12:03:06 -0700

    fix: cascade delete for pieces and composer ids

--- migrations/create_pieces.sql
+++ migrations/create_pieces.sql
@@ -4,7 +4,7 @@ CREATE TABLE pieces
     name           VARCHAR(255) NOT NULL,
     catalog_number VARCHAR(255) NOT NULL,
     composer_id    INT          NOT NULL,
-    FOREIGN KEY (composer_id) REFERENCES composers (id) ON DELETE RESTRICT ON UPDATE CASCADE
+    FOREIGN KEY (composer_id) REFERENCES composers (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_piece_name ON pieces (name);
