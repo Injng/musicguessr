95f884856db49d7ff7b871af5178fd8baafd8f34
Lin Jiang <jianglin@lnjng.com>
2025-05-02 12:01:31 -0700

    feat: reference composer_id from composers table for pieces

--- migrations/create_pieces.sql
+++ migrations/create_pieces.sql
@@ -3,7 +3,8 @@ CREATE TABLE pieces
     id             SERIAL PRIMARY KEY,
     name           VARCHAR(255) NOT NULL,
     catalog_number VARCHAR(255) NOT NULL,
-    composer       VARCHAR(255) NOT NULL
+    composer_id    INT          NOT NULL,
+    FOREIGN KEY (composer_id) REFERENCES composers (id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_piece_name ON pieces (name);
